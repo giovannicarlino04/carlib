692f82c556d4000767c9ad442d14bf858125ef2f
Giovanni Carlino <97813246+giovannicarlino04@users.noreply.github.com>
2024-12-17 13:22:30 +0100

    Fex

--- gc_time.c
+++ gc_time.c
@@ -1,5 +1,44 @@
 #include "gc_time.h"
 
+DWORD gc_ns_to_ms(LONGLONG nsec) {
+    return (DWORD)(nsec / 1000000L);
+}
+
+int gc_nanosleep(time_t sec, long nsec) {
+    LARGE_INTEGER frequency, start, current;
+    LONGLONG elapsed_ns;
+    LONGLONG total_ns = (LONGLONG)sec * BILLION + nsec;
+
+    if (nsec < 0 || nsec >= BILLION) {
+        fprintf(stderr, "Invalid nanoseconds value. Must be between 0 and 999999999.\n");
+        return -1;
+    }
+
+    if (!QueryPerformanceFrequency(&frequency)) {
+        fprintf(stderr, "High-resolution timer not available.\n");
+        return -1;
+    }
+
+    QueryPerformanceCounter(&start);
+
+    while (1) {
+        QueryPerformanceCounter(&current);
+
+        elapsed_ns = (current.QuadPart - start.QuadPart) * BILLION / frequency.QuadPart;
+
+        if (elapsed_ns >= total_ns) {
+            break;
+        }
+
+        DWORD remaining_ms = gc_ns_to_ms((LONGLONG)(total_ns - elapsed_ns));
+        if (remaining_ms > 0) {
+            Sleep(remaining_ms);
+        }
+    }
+
+    return 0; 
+}
+
 // Function to get the current time (in seconds)
 double gc_getCurrentTime() {
     return (double)clock() / CLOCKS_PER_SEC;
@@ -15,7 +54,7 @@ void gc_sleepFor(double seconds) {
     struct timespec ts;
     ts.tv_sec = (time_t)seconds;  // seconds part
     ts.tv_nsec = (long)((seconds - ts.tv_sec) * 1000000000L);  // nanoseconds part
-    nanosleep(&ts, NULL);
+    gc_nanosleep(ts.tv_sec, ts.tv_nsec);
 }
 
 // Function to create and initialize a timer
--- gc_time.h
+++ gc_time.h
@@ -2,7 +2,11 @@
 
 #include <stdio.h>
 #include <time.h>
+#include <stdio.h>
+#include <errno.h>
+#include <windows.h>
 
+#define BILLION 1000000000L
 
 #ifdef _POSIX_C_SOURCE
 #include <unistd.h>  // For usleep on Unix-like systems
@@ -22,6 +26,8 @@ typedef struct Timer {
     clock_t start_time;
 } Timer;
 
+DWORD gc_ns_to_ms(LONGLONG nsec);
+int gc_nanosleep(time_t sec, long nsec);
 double gc_getCurrentTime();
 double gc_getElapsedTime(Timer* timer);
 void gc_sleepFor(double seconds);
